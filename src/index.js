8a953c37676eed678559f372dc5aa4c0f80287ea
Yuki Soma <somay.work@gmail.com>
2021-03-16 18:24:55 +0900

    fx dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import {Provider} from './siop';
+import {saveECKey} from './keys/ec';
+import {sign} from './keys/rsa';
+import ECKey from './keys/ec';
 
 export default Provider;
+
+export {saveECKey, sign, ECKey};
