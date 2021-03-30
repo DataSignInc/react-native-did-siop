ecbc34f398f2bf3af273c06f8f88cd1d855560ea
Yuki Soma <somay.work@gmail.com>
2021-03-30 11:31:17 +0900

    remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import {Provider} from './siop';
-import ECKey, {generateKeyPair as generateECKeyPair} from './keys/ec';
 
 export default Provider;
-
-export {ECKey, generateECKeyPair};
 
 export type {SigningAlgorithm, DIDMethodPrefix, ErrorCode} from './siop-schema';
