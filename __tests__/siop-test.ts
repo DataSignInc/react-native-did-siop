26217066c6fb43f80aaec246a2f36730989055eb
Yuki Soma <somay.work@gmail.com>
2021-05-20 11:54:32 +0900

    add a request_uri test case with wider scope

--- __tests__/siop-test.ts
+++ __tests__/siop-test.ts
@@ -9,9 +9,6 @@ import {SIOPRequestValidationError} from '../src/error';
 jest.mock('../src/sioputils');
 
 describe('siop', () => {
-  const privateKeyHex =
-    '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f';
-
   const expectedIDToken =
     'eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QiLCJraWQiOiJkaWQ6ZXhhbXBsZTphYiNjb250cm9sbGVyIn0.eyJpc3MiOiJodHRwczovL3NlbGYtaXNzdWVkLm1lIiwic3ViIjoiVUNLb2FNNkk3NkpJdTQ2YkdVYUNmTVNuUXdNVUl1S21vUkYwYm5ZekxkNCIsImRpZCI6ImRpZDpleGFtcGxlOmFiIiwiYXVkIjoiaHR0cDovLzE5Mi4xNjguMC41OjUwMDEvaG9tZSIsImlhdCI6MTYxNjY2OTA0NSwiZXhwIjoxNjE2NjcyNjQ1LCJzdWJfandrIjp7Imt0eSI6IkVDIiwiY3J2IjoiSy0yNTYiLCJ4IjoiclQ2MW52dXoyTENSeng0VzFFZkV3R0FpVmdDdU42YUtUVy1QWjQ2cUQxRSIsInkiOiJBQkdsMVByNnY3blZ3dmFhMWcxNG01TTdvR2dxczIzRnBmNzgweC1WSnBNIn19.CUxOR31FRKo0RVkStp6dY3goWSKsC722b3dcAfgBKVGRuQF8GRJMgTi9WV1m_C739tN2ynT9K7IZP10iO95fvQ';
 
@@ -26,6 +23,16 @@ describe('siop', () => {
 
   test('receiveRequest() raises no errors', async () => {
     const provider = new Provider(expiresIn);
+
+    await expect(provider.receiveRequest(consts.request)).resolves.toBe(
+      consts.client_id,
+    );
+  });
+
+  test('receiveRequest() accepts request_uri', async () => {
+    const provider = new Provider(expiresIn);
+    const request = {request_uri: 'https://example.com', ...consts.request};
+    request.request = undefined;
 
     await expect(provider.receiveRequest(consts.request)).resolves.toBe(
       consts.client_id,
