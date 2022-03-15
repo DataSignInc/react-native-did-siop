42199146785763bf3dde8c2a81b865cbd8f31e9a
Yuki Soma <somay.work@gmail.com>
2022-03-15 17:58:28 +0900

    wrote up elliptic-ed25519-test.ts

--- __tests__/siop-flow/elliptic-ed25519-test.ts
+++ __tests__/siop-flow/elliptic-ed25519-test.ts
@@ -6,14 +6,7 @@ import * as consts from '../consts';
 
 import * as utils from '../../src/sioputils';
 import * as jwtModule from '../../src/jwt';
-import {
-  createJWT,
-  decodeJWT,
-  EdDSASigner,
-  Signer,
-  verifyJWS,
-  verifyJWT,
-} from 'did-jwt';
+import {EdDSASigner, Signer, verifyJWT} from 'did-jwt';
 
 jest.mock('../../src/sioputils');
 jest.mock('../../src/jwt');
@@ -31,8 +24,11 @@ describe('siop', () => {
       'V9vpz4lj1QW047t29hW28vPsYSgWJnjqPrQoPbt_x0Y',
     );
   });
+
+  const mockedNow = 1647333754;
+
   const expectedIDToken =
-    'eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QiLCJraWQiOiJkaWQ6c3R1Yjp1c2VyLTEjY29udHJvbGxlciJ9.eyJpc3MiOiJodHRwczovL3NlbGYtaXNzdWVkLm1lIiwic3ViIjoiVjl2cHo0bGoxUVcwNDd0MjloVzI4dlBzWVNnV0puanFQclFvUGJ0X3gwWSIsImRpZCI6ImRpZDpzdHViOnVzZXItMSIsImF1ZCI6Imh0dHBzOi8vZXhhbXBsZS5jb20vaG9tZSIsImlhdCI6MTYxNjY2OTA0NSwiZXhwIjoxNjE2NjcyNjQ1LCJzdWJfandrIjp7Imt0eSI6IkVDIiwiY3J2IjoiUC0yNTZLIiwieCI6IkNYZzZoNUtoa3JrY2ZIWkNIVzNVMmRWaTUta0NJRE9haTVEQUozZnI2MVkiLCJ5IjoiNmJYRWRCcU1sQTVZdlZkY3NVUDlIZ1Nvd3lMMGVIeF9MNTFDT2g5VzJ6USJ9fQ._VL3g6H49Ym7doVnlq8tz8JmvDzbZ7YCr-GSVR_mFlEDglVvafz4HGlFr51KtmymQcGHacqbqvNW9VrrDn173w';
+    'eyJhbGciOiJFZERTQSIsImtpZCI6ImRpZDpzdHViOmVkMjU1MTktdXNlci0xI2VkMjU1MTkiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2V4YW1wbGUuY29tL2hvbWUiLCJkaWQiOiJkaWQ6c3R1YjplZDI1NTE5LXVzZXItMSIsImV4cCI6MTY0NzMzNzM1NCwiaWF0IjoxNjQ3MzMzNzU0LCJpc3MiOiJodHRwczovL3NlbGYtaXNzdWVkLm1lIiwic3ViIjoiVjl2cHo0bGoxUVcwNDd0MjloVzI4dlBzWVNnV0puanFQclFvUGJ0X3gwWSIsInN1Yl9qd2siOnsiY3J2IjoiRWQyNTUxOSIsImt0eSI6Ik9LUCIsIngiOiJKNEVYX0JSTWNqUVBaOUR5TVc2RGhzN192eXNrS01uRkgtOThXWDhkUW00In19.iOPKSPj2qe8iOyo-0p38h89MOeDXq1laWpE1cc7Pkm8xFeMHRDsAxb09Qc68nPuxDEaI4iZHh9UMtJ-v-x62Cg';
 
   const initialization = {
     kid: 'did:stub:ed25519-user-1#ed25519',
@@ -55,48 +51,27 @@ describe('siop', () => {
 
   const expiresIn = 3600;
 
-  //   test('receiveRequest() raises no errors', async () => {
-  //     const provider = new Provider(expiresIn, consts.defaultResolver);
-  //     // @ts-expect-error 2339
-  //     utils.getRequestObject.mockReturnValueOnce(consts.requestJWT);
-  //     await expect(
-  //       provider.receiveRequest(consts.request),
-  //     ).resolves.toMatchObject({client_id: consts.client_id});
-  //   });
-
-  //   test('receiveRequest() accepts request_uri', async () => {
-  //     // @ts-expect-error 2339
-  //     utils.getRequestObject.mockReturnValueOnce(consts.requestJWT);
-  //     const provider = new Provider(expiresIn, consts.defaultResolver);
-  //     const request = {request_uri: 'https://example.com', ...consts.request};
-  //     request.request = undefined;
-
-  //     await expect(
-  //       provider.receiveRequest(consts.request),
-  //     ).resolves.toMatchObject({client_id: consts.client_id});
-  //   });
-
-  //   test('receiveRequest() raises errors on validation failure', async () => {
-  //     const invalidRequest = {...consts.request};
-  //     invalidRequest.response_type = 'invalid';
-  //     const provider = new Provider(expiresIn, consts.defaultResolver);
-
-  //     await expect(provider.receiveRequest(invalidRequest)).rejects.toStrictEqual(
-  //       new SIOPRequestValidationError('unsupported_response_type'),
-  //     );
-  //   });
-
   test('generate ID Token', async () => {
     const provider = new Provider(expiresIn, consts.defaultResolver);
 
     // @ts-expect-error 2322
-    utils.getIssuedAt.mockReturnValueOnce(1647333754);
+    utils.getIssuedAt.mockReturnValueOnce(mockedNow);
+
+    await expect(
+      provider.generateIDToken(consts.requestObject, persona),
+    ).resolves.toBe(expectedIDToken);
+  });
+
+  test('able to verify ID token', async () => {
+    const provider = new Provider(expiresIn, consts.defaultResolver);
+
+    // @ts-expect-error 2322
+    utils.getIssuedAt.mockReturnValueOnce(mockedNow);
 
     const idToken = await provider.generateIDToken(
       consts.requestObject,
       persona,
     );
-    console.log(idToken);
 
     const resolver = async (did: string) => {
       return {
@@ -121,65 +96,20 @@ describe('siop', () => {
       };
     };
 
-    // const jwt = await createJWT(
-    //   {test: 'test'},
-    //   {
-    //     alg: 'EdDSA',
-    //     issuer: 'did:stub:ed25519-user-1',
-    //     signer: EdDSASigner(consts.ed25519User.privateKeyHex),
-    //     canonicalize: true,
-    //   },
-    // );
-
-    console.log(await decodeJWT(idToken));
-
-    await verifyJWT(idToken, {
-      resolver: {resolve: resolver},
-      audience: 'https://example.com/home',
-    });
-
-    // await expect(idToken).resolves.toBe(expectedIDToken);
+    await expect(
+      verifyJWT(idToken, {
+        resolver: {resolve: resolver},
+        audience: consts.client_id,
+      }),
+    ).resolves.toBeDefined();
   });
-
-  //   test('generate ID Token with additionalFields', async () => {
-  //     const provider = new Provider(expiresIn, consts.defaultResolver);
-  //     // mock the sign() method
-  //     persona.sign = (payload: any) => payload;
-
-  //     // @ts-expect-error 2322
-  //     utils.getIssuedAt.mockReturnValueOnce(1616669045);
-  //     await expect(
-  //       provider.generateIDToken(consts.requestObject, persona, {
-  //         test: 'test',
-  //         test2: 2,
-  //         iss: 'invalid',
-  //       }),
-  //     ).resolves.toMatchObject({
-  //       aud: 'https://example.com/home',
-  //       did: 'did:stub:user-1',
-  //       exp: 1616672645,
-  //       iat: 1616669045,
-  //       iss: 'https://self-issued.me',
-  //       nonce: undefined,
-  //       state: undefined,
-  //       sub: 'V9vpz4lj1QW047t29hW28vPsYSgWJnjqPrQoPbt_x0Y',
-  //       sub_jwk: {
-  //         kty: 'OKP',
-  //         crv: 'Ed25519',
-  //         // base64url-encoded public key.
-  //         x: 'THP2m9FHt3YXeQTSNeJBtJq3HVL7E9L7-pmuSJCqMkg',
-  //       },
-  //       test: 'test',
-  //       test2: 2,
-  //     });
-  //   });
 
   test('generate response', async () => {
     const provider = new Provider(expiresIn, consts.defaultResolver);
     // @ts-expect-error 2322
     utils.getRequestObject.mockReturnValueOnce(consts.requestJWT);
     // @ts-expect-error 2322
-    utils.getIssuedAt.mockReturnValueOnce(1616669045);
+    utils.getIssuedAt.mockReturnValueOnce(mockedNow);
     await provider.receiveRequest(consts.request);
 
     await expect(
